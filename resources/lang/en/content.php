05175ddb689dd96273bf6b67e5943577a4834cb7
Andrei Ioniță <hi@andrei.io>
2019-07-30 14:10:45 +0300

    Update placeholder strings

--- resources/lang/en/content.php
+++ resources/lang/en/content.php
@@ -11,8 +11,8 @@ return [
     'materials'               => 'Materials',
 
     'submit'                  => 'Submit',
-    'newsletterThanksTitle'   => 'newsletterThanksTitle',
-    'newsletterThanksContent' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero debitis quidem repellat odit, quis inventore suscipit tempora beatae totam. Rem beatae quisquam laborum iste ipsam.',
+    'newsletterThanksTitle'   => 'Thank you!',
+    'newsletterThanksContent' => 'Please check your email to confirm your subscription.',
 
     'cookies'                 => 'In order to provide you with the best browsing experience we use cookies. If you disagree with this, you may withdraw your consent by changing the settings on your browser.',
     'cookiesMore'             => 'More info',
