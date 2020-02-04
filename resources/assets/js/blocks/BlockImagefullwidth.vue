16d4fd71f7401934dd1e447876338dd16f6c9dd2
Andrei Ioniță <hi@andrei.io>
2020-02-04 18:56:26 +0000

    Regenerate blocks to match new toolbar options (#62)

--- resources/assets/js/blocks/BlockImagefullwidth.vue
+++ resources/assets/js/blocks/BlockImagefullwidth.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- eslint-disable -->
     <div class="block__body">
-        <a17-inputframe label="Image" name="medias.image" :required="true" > <a17-mediafield :name="fieldName('image')" crop-context="image" :required="true" :with-video-url="false" ></a17-mediafield> </a17-inputframe>
+        <a17-inputframe label="Image" name="medias.image" :required="true" > <a17-mediafield :name="fieldName('image')" crop-context="image" :required="true" :with-video-url="false" ></a17-mediafield> </a17-inputframe> 
     </div>
 </template>
 
