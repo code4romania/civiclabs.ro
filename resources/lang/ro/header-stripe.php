aedcc7b797386726380978dd7b80ef5f42d78a01
genuineq <47278224+cosminbosutar@users.noreply.github.com>
2020-02-13 18:51:34 +0200

    Fix pages layout (#66)
    Co-authored-by: bosutarcosmin <bosutar.cosmin@gmail.com>
    Co-authored-by: Andrei Ioniță <hi@andrei.io>

--- resources/lang/ro/header-stripe.php
+++ resources/lang/ro/header-stripe.php
@@ -2,8 +2,9 @@
 
 return [
 
-    'support'   => 'Susținut de',
+    'projectby'    => 'Un program al',
+    'support'      => 'Susținut de',
 
-    'projectby' => 'Apel pe proiecte Fondul pentru cutremur și alte dezastre majore Bucureștiul',
-    'initiator' => 'Pregătit și inițiat de',
+    'project-type' => 'Apel pe proiecte Fondul pentru cutremur și alte dezastre majore Bucureștiul',
+    'initiator'    => 'Pregătit și inițiat de',
 ];
