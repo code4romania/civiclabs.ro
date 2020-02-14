1549cc42c6f92451f27dde9c5cf6c4a3033413d9
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-02-14 18:23:40 +0200

    Changed the message from the second sponsors stripe.

--- resources/lang/en/header-stripe.php
+++ resources/lang/en/header-stripe.php
@@ -4,7 +4,6 @@ return [
 
     'projectby'    => 'A programme by',
     'support'      => 'Powered by',
-  
-    'project-type' => 'Project calls Fondul pentru cutremur și alte dezastre majore Bucureștiul',
-    'initiator'    => 'Prepared and initiated by',
+
+    'project-type' => 'Project calls, Fondul pentru cutremur și alte dezastre majore Bucureștiul Pregătit initiated by',
 ];
