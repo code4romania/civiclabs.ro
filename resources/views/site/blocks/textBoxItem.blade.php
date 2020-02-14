a16b27dddbdc780d72d1b4575200ef977ea7f6ee
genuineq <47278224+cosminbosutar@users.noreply.github.com>
2020-02-14 12:24:46 +0200

    Extend textboxes (#67)
    Co-authored-by: bosutarcosmin <bosutar.cosmin@gmail.com>
    Co-authored-by: Andrei Ioniță <hi@andrei.io>

--- resources/views/site/blocks/textBoxItem.blade.php
+++ resources/views/site/blocks/textBoxItem.blade.php
@@ -7,7 +7,14 @@
             <h1 class="title is-size-5">{{ $block->translatedinput('header') }}</h1>
         </div>
     </div>
+
     <div class="content">
         {!! $block->translatedinput('description') !!}
     </div>
+
+    @if ($block->input('button_text') && $block->input('button_url'))
+        <a target="_blank" rel="noopener noreferrer" href="{{ $block->translatedinput('button_url') }}" class="button is-success is-medium is-fullwidth">
+            {{ $block->translatedinput('button_text') }}
+        </a>
+    @endif
 </article>
