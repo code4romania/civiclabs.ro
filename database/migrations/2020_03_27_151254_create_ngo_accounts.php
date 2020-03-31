9250149f34ed63d5fda1ea7371ac999fc1d34172
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-03-31 15:24:58 +0300

    Added 'status' column for 'application_submissions' table

--- database/migrations/2020_03_27_151254_create_ngo_accounts.php
+++ database/migrations/2020_03_27_151254_create_ngo_accounts.php
@@ -14,14 +14,19 @@ class CreateNgoAccounts extends Migration
      */
     public function up()
     {
+        /** Add 'applicant' as a user_role type. */
+        DB::statement("ALTER TABLE dashboard_users CHANGE COLUMN user_role user_role ENUM('financer', 'evaluator', 'applicant')");
+
+        /** Link 'application_submissions' table to 'dashboard_users' table. */
         Schema::table('application_submissions', function (Blueprint $table) {
             $table->integer('dashboard_user_id')->unsigned()->nullable();
             $table->foreign('dashboard_user_id')->references('id')->on('dashboard_users')->onDelete('cascade');
 
+            $table->enum('status', ['received', 'evaluation', 'rejected'])->nullable();
+
             $table->unique(['solution_id', 'dashboard_user_id']);
         });
 
-        DB::statement("ALTER TABLE dashboard_users CHANGE COLUMN user_role user_role ENUM('financer', 'evaluator', 'applicant')");
     }
 
     /**
@@ -31,15 +36,19 @@ class CreateNgoAccounts extends Migration
      */
     public function down()
     {
+        /** Remove link between 'application_submissions' table and 'dashboard_users' table. */
         Schema::table('application_submissions', function (Blueprint $table) {
-            $table->dropForeign('application_submissions_dashboard_user_id_foreign');
             $table->dropIndex('application_submissions_solution_id_dashboard_user_id_unique');
+
+            $table->dropColumn('status');
+
+            $table->dropForeign('application_submissions_dashboard_user_id_foreign');
             $table->dropColumn('dashboard_user_id');
         });
 
         /** Delete all applicant accounts. */
-        DashboardUser::where('user_role', 'applicant')->delete();
-
+        DB::statement("DELETE FROM `dashboard_users` WHERE user_role='applicant'");
+        /** Remove 'applicant' as a user_role type. */
         DB::statement("ALTER TABLE dashboard_users CHANGE COLUMN user_role user_role ENUM('financer', 'evaluator')");
     }
 }
