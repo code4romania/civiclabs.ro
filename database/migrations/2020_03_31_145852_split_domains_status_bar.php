c46204910e239ce99795b18eb939c6f41ebae96a
cosmin_bosutar <bosutar.cosmin@gmail.com>
2020-03-31 23:06:19 +0300

    Created migration to add 'research_percentage' and 'prototyping_percentage' columns to the 'domains' table.

---
+++ database/migrations/2020_03_31_145852_split_domains_status_bar.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SplitDomainsStatusBar extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /** Link 'application_submissions' table to 'dashboard_users' table. */
+        Schema::table('domains', function (Blueprint $table) {
+            $table->integer('research_percentage')->unsigned()->nullable();
+            $table->integer('prototyping_percentage')->unsigned()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        /** Remove link between 'application_submissions' table and 'dashboard_users' table. */
+        Schema::table('domains', function (Blueprint $table) {
+            $table->dropColumn('prototyping_percentage')->unsigned()->nullable();
+            $table->dropColumn('research_percentage')->unsigned()->nullable();
+        });
+    }
+}
