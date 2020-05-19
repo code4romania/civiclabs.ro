b6c4a72c558bad318609b7dd660c69da9067bcbb
Andrei Ioniță <hi@andrei.io>
2020-05-19 19:04:27 +0100

    Add optional evaluation section description (#83)

--- resources/views/admin/blocks/evalSection.blade.php
+++ resources/views/admin/blocks/evalSection.blade.php
@@ -6,6 +6,14 @@
     'translated'     => true,
 ])
 
+@formField('wysiwyg', [
+    'name'           => 'description',
+    'label'          => 'Description',
+    'toolbarOptions' => config('twill.toolbar_options'),
+    'editSource'     => true,
+    'translated'     => true,
+])
+
 @formField('repeater', [
     'type' => 'evalField',
 ])
