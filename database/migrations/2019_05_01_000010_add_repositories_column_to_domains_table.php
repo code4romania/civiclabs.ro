70de78c662629753aede17c79b5924f7b3450405
Andrei Ioniță <hi@andrei.io>
2020-07-25 22:23:12 +0100

    Rename other possibly conflicting migrations

--- database/migrations/2019_05_01_000010_add_repositories_column_to_domains_table.php
+++ database/migrations/2019_05_01_000010_add_repositories_column_to_domains_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class UpdateSolutionsTables extends Migration
+class AddRepositoriesColumnToDomainsTable extends Migration
 {
     public function up()
     {
--- database/migrations/2020_03_31_145852_add_status_columns_to_domains_table.php
+++ database/migrations/2020_03_31_145852_add_status_columns_to_domains_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SplitDomainsStatusBar extends Migration
+class AddStatusColumnsToDomainsTable extends Migration
 {
     /**
      * Run the migrations.
